b70c16ed1f0a4e507bf3b404fd714f59370941f9
etsraphael <ets_raphael@outlook.fr>
2021-12-03 20:22:36 +1000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import Web3 from 'web3'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-tour-of-heroes';
+
+  ngOnInit(): void {
+
+    console.log(Web3)
+
+    // if (typeof window.ethereum !== 'undefined') {
+    //   console.log(window.ethereum)
+    //   console.log('MetaMask is installed!');
+    // }
+
+    // let web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
+    // console.log(web3);
+  }
 }
