44c340ec66cbada657d028d8ae889c57a8bec475
etsraphael <ets_raphael@outlook.fr>
2021-12-19 01:51:13 +1000

    save for today

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,15 @@ import { Web3Service } from './core/service/web3/web3.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  title = 'angular-tour-of-heroes';
-  web3: Web3|any;
+  web3: Web3 = null!;
+  accountConnected: string = null!;
 
-  constructor(
-    private web3Service: Web3Service,
-  ) {}
+  constructor(private web3Service: Web3Service) {}
 
   ngOnInit(): void {
     this.web3 = this.web3Service.checkIfWeb3IsInstalled();
     this.getAccount();
+    this.listener();
   }
 
   login(): void {
@@ -25,8 +24,7 @@ export class AppComponent implements OnInit {
       window.ethereum
         .request({ method: 'eth_requestAccounts' })
         .then(() => {
-          console.log('requested accounts successful');
-          this.getAccount()
+          this.getAccount();
         })
         .catch(() => {
           console.log('requested accounts error');
@@ -35,20 +33,24 @@ export class AppComponent implements OnInit {
   }
 
   getAccount(): void {
-    this.web3.eth.getAccounts().then(console.log);
+    this.web3.eth.getAccounts().then(
+      (response: string[]) => {
+        this.accountConnected = response[0];
+      },
+      () => {
+        console.log('get accounts error');
+      }
+    );
   }
 
-  checkIfWeb3IsInstalled(): Web3 {
-    if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
-      // Use web3 provider
-      return new Web3(window.web3.currentProvider);
-    } else {
-      console.log('No web3? You should consider trying MetaMask!');
-      // Hack to provide backwards compatibility for Truffle, which uses web3js 0.20.x
-      Web3.providers.HttpProvider.prototype.sendAsync =
-        Web3.providers.HttpProvider.prototype.send;
-      // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-      return new Web3(new Web3.providers.HttpProvider(Web3.givenProvider));
-    }
+  logout(): void {
+    this.accountConnected = null!;
+  }
+
+  listener() {
+    window.ethereum.on('accountsChanged', async () => {
+      // Do something
+      alert('accounts changed');
+    });
   }
 }
