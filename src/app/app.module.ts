9928c185b373deb71ae4b6211a7a026e4db39748
etsraphael <ets_raphael@outlook.fr>
2022-02-01 23:03:08 +1000

    page created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignComponent } from './page/sign/sign.component';
+import { HomeComponent } from './page/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
