deb620ded63cd70ff1fd7711b78f9f9c0bc0dc7f
etsraphael <ets_raphael@outlook.fr>
2022-02-01 23:09:39 +1000

    routing is working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './page/home/home.component';
+import { SignComponent } from './page/sign/sign.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  // by default
+  { path: '', redirectTo: 'sign', pathMatch: 'full' },
+
+  // sign in page
+  {
+    path: 'sign',
+    component: SignComponent,
+  },
+
+  // home page
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
